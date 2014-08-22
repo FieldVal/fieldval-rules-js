ac7e0b85a00ab87581d53d5656746eb99abb7be5
marcuslongmuir <marcuslongmuir@me.com>
2014-08-22 17:31:18 +0100

    Fixed choices field

--- fieldval-rules.js
+++ fieldval-rules.js
@@ -642,7 +642,9 @@ function ChoiceRuleField(json, validator) {
 ChoiceRuleField.prototype.create_ui = function(parent){
     var field = this;
 
-    field.ui_field = new ChoiceField(field.display_name || field.name, field.choices, field.json);
+    field.json.choices = field.choices;
+
+    field.ui_field = new ChoiceField(field.display_name || field.name, field.json);
     field.container = field.ui_field.container;
     parent.add_field(field.name, field);
     return field.ui_field;
@@ -668,6 +670,8 @@ ChoiceRuleField.prototype.focus = function(){
 ChoiceRuleField.prototype.init = function() {
     var field = this;
 
+    field.allow_empty = field.validator.get("allow_empty", BasicVal.boolean(false));
+    field.empty_message = field.validator.get("empty_message", BasicVal.string(false));
     field.choices = field.validator.get("choices", BasicVal.array(true));
 
     return field.validator.end();
