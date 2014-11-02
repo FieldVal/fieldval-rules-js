1d5f57f291e2268707203deb29c2c7506a35a1eb
Marcus Longmuir <marcuslongmuir@me.com>
2014-11-02 16:09:23 +0000

    Added ability to create TextField UI as a textarea

--- fieldval-rules.js
+++ fieldval-rules.js
@@ -197,7 +197,16 @@ function TextRuleField(json, validator) {
 TextRuleField.prototype.create_ui = function(parent){
     var field = this;
 
-    field.ui_field = new TextField(field.display_name || field.name, field.json);
+    var type = field.json.type;
+    if(field.json.textarea===true){
+        type = "textarea";
+    }
+
+    field.ui_field = new TextField(field.display_name || field.name, {
+        name: field.json.name,
+        display_name: field.json.display_name,
+        type: type
+    });
     field.element = field.ui_field.element;
     parent.add_field(field.name, field);
     return field.ui_field;
@@ -218,6 +227,9 @@ TextRuleField.prototype.init = function() {
         field.checks.push(BasicVal.max_length(field.max_length,{stop_on_error:false}));
     }
 
+    field.textarea = field.validator.get("textarea", BasicVal.boolean(false));
+
+    //Currently unused
     field.phrase = field.validator.get("phrase", BasicVal.string(false));
     field.equal_to = field.validator.get("equal_to", BasicVal.string(false));
     field.ci_equal_to = field.validator.get("ci_equal_to", BasicVal.string(false));
