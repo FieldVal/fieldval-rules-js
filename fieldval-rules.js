d15bd106b71734f86cb4ec6821158ba4944f8e1f
bestan <bestan93@gmail.com>
2015-03-18 12:20:08 +0200

    Implemented advanced choice_field editor ui

--- fieldval-rules.js
+++ fieldval-rules.js
@@ -967,19 +967,124 @@ var FVChoiceRuleField = (function(){
     FVChoiceRuleField.add_editor_params = function(editor) {
         var field = this;
 
-        var choices_array_field = new FVArrayField("Choices");
-        choices_array_field.new_field = function() {
-            return new FVTextField();
+        var value = editor.val();
+
+        var choices_field = new FVObjectField("Choices");
+
+        var id = 0;
+
+        var create_choices_editor_array  =function () {
+            id+=1;
+            var choices = new FVArrayField();
+            choices.id = id;
+            choices.new_field = function() {
+                var choices = this;
+                if (mode_field.val() == "simple") { 
+                    return new FVTextField("Value");
+                } else {
+                    var tuple = new FVArrayField("", {
+                        hide_remove_button: true, 
+                        hide_add_button: true,
+                        sortable: false
+                    })
+                    tuple.add_field(new FVTextField("Value"));
+                    tuple.add_field(new FVTextField("Display name"));
+                    return tuple;
+                }
+            }
+            choices.val = function(set_val, options) {
+                var choices = this;
+                if (set_val) {
+                    var transformed_choices = [];
+                    for (var i=0; i<set_val.length; i++) {
+                        var old_choice = set_val[i];
+                        if (mode_field.val() == "simple") {
+                            if (Array.isArray(old_choice)) {
+                                transformed_choices.push(old_choice[0])
+                            } else {
+                                transformed_choices.push(old_choice);
+                            }
+                        } else {
+                            if (Array.isArray(old_choice)) {
+                                transformed_choices.push(old_choice)
+                            } else {
+                                transformed_choices.push([old_choice, old_choice]);
+                            }
+                        }
+                    }
+                    return FVArrayField.prototype.val.call(choices, transformed_choices, options);
+
+                } else {
+                    var original_choices = FVArrayField.prototype.val.apply(choices, arguments);
+                    var transformed_choices = [];
+                    for (var i=0; i<original_choices.length; i++) {
+                        var choice = original_choices[i];
+                        if (Array.isArray(choice) && choice[0] == choice[1]) {
+                            transformed_choices.push(choice[0])
+                        } else {
+                            transformed_choices.push(choice);
+                        }
+                    }
+                    return transformed_choices;
+                }
+
+            }
+            return choices;
         }
 
-        editor.add_field("choices", choices_array_field);
+        
+        var mode_field = new FVChoiceField("Mode", {choices: ["simple", "advanced"]}).on_change(function(val) {
+            var old_choices = [];
+            if (choices_field.fields.choices) {
+                old_choices = choices_field.fields.choices.val();
+            }
+            choices_field.remove_field("choices");
+            
+            var choices = create_choices_editor_array();
+            choices_field.add_field("choices", choices);
+            choices.val(old_choices);
+        })
+
+        choices_field.add_field("mode", mode_field);
+
+        choices_field.update_mode = function(choices) {
+            var is_simple = true;
+            if (choices) {
+                for (var i=0; i<choices.length; i++) {
+                    if (Array.isArray(choices[i])) {
+                        is_simple = false;
+                        break;
+                    }
+                }
+            }
+
+            if (is_simple) {
+                mode_field.val("simple");
+            } else {
+                mode_field.val("advanced");
+            }
+        }
+
+        choices_field.val = function() {
+            if (arguments.length >0 && arguments[0]) {
+                choices_field.update_mode(arguments[0]);
+            }
+
+            var choices = choices_field.fields.choices;
+            return choices.val.apply(choices, arguments);
+        }
+
+        choices_field.update_mode(value.choices);
+        choices_field.val(value.choices);
+
+        editor.add_field("choices", choices_field);
+
         editor.add_field("allow_empty", new FVBooleanField("Allow empty"));
         editor.add_field("empty_message", new FVTextField("Empty message"));
 
-        var value = editor.val();
-        editor.fields.choices.val(value.choices);
         editor.fields.allow_empty.val(value.allow_empty);
         editor.fields.empty_message.val(value.empty_message);
+
     }
 
     return FVChoiceRuleField;
